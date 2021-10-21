a3e467bbeff5434a7f9b2623722b634baf6c187b
Sqwyer <sawyer.codes.stuff@gmail.com>
2021-10-21 14:56:57 -0500

    Delete v1 from src

--- src/v1/mod.ts
+++
@@ -1,9 +0,0 @@
-import index = require("./std/index");
-import server = require("./std/server");
-
-export = {
-    Client: index.default.Client,
-    std: {
-        Server: server.default.Server
-    }
-};
